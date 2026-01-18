5a856e9a3c8712f5aca6c7de31aa5458de038415
ForgottenScream <forgottenscream@noreply.codeberg.org>
2026-01-18 09:58:28 +0000

    added clock to count FPS, saving to dt variable

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -13,6 +15,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
